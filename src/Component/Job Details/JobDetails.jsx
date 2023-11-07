a7d256958d27ebf913d0355a6837cfa5777fa7cb
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2023-11-07 19:26:45 +0600

    job bid function added

--- src/Component/Job Details/JobDetails.jsx
+++ src/Component/Job Details/JobDetails.jsx
@@ -9,7 +9,39 @@ const JobDetails = () => {
 
   const findJob = jobDetails.find((item) => item._id === params.id);
 
- 
+  const HandleJobBid = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const bidAmount = form.bidAmount.value;
+    const deadline = form.deadline.value;
+    const jobOwner = form.jobOwner.value;
+    const bidBy = form.bidBy.value;
+    const jobId = params.id;
+
+    const sellerBid = {
+      bidAmount: bidAmount,
+      deadline: deadline,
+      jobOwner: jobOwner,
+      bidBy: bidBy,
+      jobId: jobId,
+    };
+
+    console.log(sellerBid)
+
+    axios
+    .post("http://localhost:5000/bids", sellerBid)
+    .then((res) => {
+      console.log(res.data);
+      if(res.data.insertedId){
+          swal("Hurray!", "Your job post has been posted successfully!", "success");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  };
 
 
 
